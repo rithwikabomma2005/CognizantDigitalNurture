6b014f4b4f08adf99d59bab04eacb5bcac5c2e2a
rithwikabomma2005 <2200031438cseh@gmail.com>
2025-07-30 17:10:35 +0530

    w6

---
+++ Week6/React/cohorttracker/src/CohortDetails.js
@@ -0,0 +1,19 @@
+import styles from './CohortDetails.module.css';
+<div className={styles.box}>
+  <h3 style={{ color: cohort.status === 'Ongoing' ? 'green' : 'blue' }}>
+    {cohort.name}
+  </h3>
+  <dl>
+    <dt>Started On</dt>
+    <dd>{cohort.startDate}</dd>
+
+    <dt>Current Status</dt>
+    <dd>{cohort.status}</dd>
+
+    <dt>Coach</dt>
+    <dd>{cohort.coach}</dd>
+
+    <dt>Trainer</dt>
+    <dd>{cohort.trainer}</dd>
+  </dl>
+</div>
